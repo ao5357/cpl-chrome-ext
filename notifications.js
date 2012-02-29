cd4767bced5e01439e33dc2c03ed37e094bdb9fe
Brad Czerniak <ao5357@gmail.com>
2012-02-29 16:58:05 -0500

    Moved Language zone checking to the regex and am using the verbatim jsfiddle ISBN validator

--- notifications.js
+++ notifications.js
@@ -1,5 +1,5 @@
-// ISBN validator with slight mods: http://jsfiddle.net/ao5357/kcN2f/
-String.prototype.valI = function(){var a=this,b=0,i=0,l=a.length;if(l==10 && (a[0] == 0 || a[0] == 1)){var c=a[9]=='X'?10:a[9]*1;for(i;i<9;i++){b+=a[i]*(10-i)}return (b+c)%11==0?a:0}if(l==13){var c=a[12]==0?10:a[12]*1;for(i;i<12;i++){b+=(i+1)%2==0?a[i]*3:a[i]*1}return 10-(b%10)==c?a:0}};
+// Tiny, ugly ISBN validator: http://jsfiddle.net/ao5357/kcN2f/
+function v(a){var b=i=r=0,t=10,l=a.length;if(l==t){for(i;i<9;i++)b+=a[i]*(t-i);r=(b+(a[9]=='X'?t:a[9]))%11==0}if(l==13){for(i;i<12;i++)b+=(i+1)%2?+a[i]:a[i]*3;r=b%t==t-(+a[12]||t)}return r?a:0}
 
 // sort descending: takes original order into account [as much as possible]
 function sortProps(a,b){
@@ -18,12 +18,12 @@ function flipSort(obj){
 // find isbn-like numbers on the page
 var isbns = new Object(), contents = document.body.innerHTML, badMatches = ["0000000000","0123456789"];
 contents = contents.replace(/[\- \.]/ig,"");
-contents = contents.match(/(97[89][0-9]{10}|[0-9]{10}|[0-9]{9}[xX])/ig);
+contents = contents.match(/(97[89][0-9]{10}|[01][0-9]{9}|[01][0-9]{8}[xX])/ig);
 for(var i in contents){
-	if(contents[i].valI() && !isbns[contents[i]] && badMatches.indexOf(contents[i]) == -1){
+	if(v(contents[i]) && !isbns[contents[i]] && badMatches.indexOf(contents[i]) == -1){
 		isbns[contents[i]] = 1;
 		}
-	else if(contents[i].valI() && isbns[contents[i]]){
+	else if(v(contents[i]) && isbns[contents[i]]){
 		isbns[contents[i]]++;
 		}
 	}
